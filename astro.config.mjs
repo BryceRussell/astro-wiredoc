2a58eed80fee234dff060a56083ad63225099586
BryceRussell <19967622+BryceRussell@users.noreply.github.com>
2022-12-19 23:01:47 -0600

    Fix styles for safari, make button icons smaller

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,28 @@
 import { defineConfig } from 'astro/config';
-
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import { defaultFrontmatter } from 'astro-default-frontmatter';
+import { h } from 'hastscript'
 
 export default defineConfig({
     site: process.env.DEV
         ? 'https://localhost:3000/'
         : 'https://localhost:3000/',
     markdown: {
+        extendDefaultPlugins: true,
         remarkPlugins: [
             [ defaultFrontmatter, {
                 layout: "@/layouts/Markdown.astro"
+            }]
+        ],
+        rehypePlugins: [
+            [rehypeSlug, {}],
+            [rehypeAutolinkHeadings, {
+                behavior: 'prepend',
+                test: ['h2', 'h3', 'h4', 'h5', 'h6'],
+                content: [
+                    h('span.heading-link-icon', '#')
+                ]
             }]
         ]
     }
