d8e9869ca7a9a2b6ce1c81f2ea05497b0bd703a5
BryceRussell <19967622+BryceRussell@users.noreply.github.com>
2022-12-19 23:04:11 -0600

    fix seo

--- src/config.ts
+++ src/config.ts
@@ -6,18 +6,18 @@ export const SITE = 'https://localhost:3000/'
 export const SEODefault = (Astro: AstroGlobal): SEOInterface => {
     if (!Astro.site) throw Error("src/config.ts SEODefault(): No 'site' option is set in astro.config.mjs")
     return {
-        title: "Fast Docs",
-        description: "Fast docs is a fast easy to use documentation template built using Astro",
+        title: "Astro Simple Docs",
+        description: "A fast and easy to use documentation template built using Astro",
         canonical: ''+(new URL(Astro.url.pathname, Astro.site)),
         openGraph: {
             basic: {
-                title: "Fast Docs Template",
+                title: "Simple Docs",
                 url: ''+(new URL(Astro.url.pathname, Astro.site)),
                 image: "/og.webp",
                 type: "website"
             },
             optional: {
-                siteName: "Fast Docs",
+                siteName: "Astro Simple Docs",
                 description: "Documentation template built using Astro"
             },
             image: {
